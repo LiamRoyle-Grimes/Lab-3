8a66706b5e587a0dde13d909430f05ef22b46d93
LiamRoyle-Grimes <141082592+LiamRoyle-Grimes@users.noreply.github.com>
2025-05-19 05:00:35 -0600

    Add files via upload

--- lab 3/untitled1.py
+++ lab 3/untitled1.py
@@ -12,7 +12,7 @@ import astropy.units as u
 import astropy.constants as const
 
 from scipy.optimize import curve_fit as fit
-
+from scipy.stats import chisquare
 
 def linear(x,m,b):
     return m*x+b
@@ -21,6 +21,14 @@ def linear(x,m,b):
 t,T,v=np.loadtxt("BBDATA.txt",skiprows=5,unpack=True)
 
 T+=273.15
+
+
+fig,ax=plt.subplots()
+ax.plot(T,v)
+ax.set_xlabel("T K")
+ax.set_ylabel("Voltage mV")
+fig.show()
+
 
 Tm=np.argmax(T)
 tup=t[:Tm]
@@ -33,33 +41,34 @@ Td=T[Tm:]
 vd=v[Tm:]
 xd= Td**4-T[0]**4
 
-plt.plot(xup,vup)
-plt.plot(xd,vd)
-plt.show()
+fig1, ax1=plt.subplots()
+ax1.plot(xup,vup)
+ax1.plot(xd,vd)
+fig1.show()
 
 parup,covup=fit(linear,xup,vup)
 
-plt.plot(xup,linear(xup,parup[0],parup[1]),label=f"{parup[0]:2g}$(T^4-T_0^4)$ + {parup[1]:2f}")
+ax1.plot(xup,linear(xup,parup[0],parup[1]),label=f"{parup[0]:2.2g}$(T^4-T_0^4)$ + {parup[1]:2.2g}")
 
 pard,covd=fit(linear,xd,vd)
 
-plt.plot(xd,linear(xd,pard[0],pard[1]),label=f"{pard[0]:2g}$(T^4-T_0^4)$ + {pard[1]:2f} ")
+ax1.plot(xd,linear(xd,pard[0],pard[1]),label=f"{pard[0]:2.2g}$(T^4-T_0^4)$ + {pard[1]:2.2g} ")
 
-plt.xlabel("$T^4-T_0^4$")
-plt.ylabel("$mV$")
-plt.ylabel
+
+
+
+ax1.set_xlabel("$T^4-T_0^4$")
+ax1.set_ylabel("$mV$")
 
 plt.legend()
+plt.show()
 
 
 
+print(parup,np.sqrt(np.diag(covup)))
+print(chisquare(vup,linear(xup,parup[0],parup[1]),ddof=2))
 
-x=T**4-T[0]**4
 
-
-'''plt.plot(x,v)
-plt.plot(x,0.6/2e10*x-0.03)
-plt.plot(x,0.6/2e10*x+0.05)
-plt.plot(x,0.6/2e10*x+0.12)
-#plt.plot(x,0.45/3e8*x+0.32)'''
+print(pard,np.sqrt(np.diag(covd)))
+print(chisquare(vd,linear(xd,pard[0],pard[1]),ddof=2))
 
